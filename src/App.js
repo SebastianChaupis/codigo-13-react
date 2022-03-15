20ed308f69c08d492a7d2b7d1a48e260ec5ef4b3
SebastianChaupis <sebaschaupis1@gmail.com>
2022-03-15 15:54:09 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
 //Crear componente desde 0
+const PrimerComponente = ()=>{
+  return(
+      <div>
+          <h1>Hola mundo</h1>
+      </div>
+    );
+};
+//Luego de crear el componente debemos exportarlo
+//Default es como decir que este archivo unicamente esta exportando este componente
+export default PrimerComponente;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppInicial from './App';
 import reportWebVitals from './reportWebVitals';
+
+import PrimerComponente from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppInicial />
+    <PrimerComponente />
   </React.StrictMode>,
   document.getElementById('root')
 );
