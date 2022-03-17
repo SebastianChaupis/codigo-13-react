0c142076475ea55bd0349687cfad483792969d49
SebastianChaupis <sebaschaupis1@gmail.com>
2022-03-17 18:25:06 -0500

    pokeAPI

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,58 @@
-import { Container } from "@mui/material";
-const Home =()=>{
-    return(
+import { useState, useEffect } from "react";
+import { Container, Card, CardContent, CardMedia, Grid } from "@mui/material";
+
+import { getDataFromPokemon } from "../../service";
+//vamos a ejecutar la funcion que se encargara de traer los pokemones
+
+const Home = () => {
+    const imgUrl =
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
+    //vamos a crear una variable que se encague de guardar la lista de pokemones
+    const [pokemones, setPokemon] = useState([]);
+    //creamos una funcion la cual se encarga de ejecutar la funcion getDataFromPokemon y 
+    //la data que retorne la funcion guardarla usando setPokemon
+    const fetchPokemonList = async () => {
+        const listPokemones = await getDataFromPokemon();
+        //ahora como ya recuperamos la lista de los pokemones hay que usar la funcion
+        // setPokemon para poder guardar la lista de pokemones
+        console.log("listPokemones.results", listPokemones.results);
+        setPokemon(listPokemones.results);
+    };
+    //En React existe una funcion llamada useEffect la cual se ejecuta cada vez que se carga la pagina, vamos 
+    // a usar useEffect si queremos ejecutar algo al inicio de la aplicacion
+    useEffect(() => {
+        //aca llamamos la funcion que se quiere ejecutar
+        fetchPokemonList();
+        // IMPORTANTE AL EJECUTAR "useEffect"  DEBEMOS COLOCAR UN ARRAY VACION PARA EVITAR QUE SE HAGA UN BUCLE
+        //INFINITO, SI NO COLOCAMOS EL ARRAY VACIO LA FUNCION QUE EJECUTA "useEffect" SE EJECTURAR INFINITAS VECES 
+    }, []);
+
+    return (
         <Container>
-            <h1>Mi pagina React</h1>
+            <h1>Pokedex</h1>
+            {/*pokemons.lenght > 0 && "lleno" ----------> if*/}
+            {/*pokemons.lenght > 0 ? "lleno" : "vacio" ----------> if else*/}
+            <Grid container spacing={5}>
+                {pokemones.length > 0 &&
+                    pokemones.map((pokemon, index) => (
+                        <Grid item md={4}>
+                            <Card sx={{width:300, height: 500 }}>
+                                <CardMedia component="img" image={`${imgUrl}${index + 1}.svg`} />
+                                <CardContent>
+                                    <h3>{pokemon.name}</h3>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))
+                }
+
+            </Grid>
+            ) : (
+            <h1>No hay datos</h1>
+            )
+
+
         </Container>
     );
-}
+};
 export default Home;
--- src/service/index.js
+++ src/service/index.js
@@ -3,17 +3,20 @@ Este sera nuestro archivo donde almacenaremos nuestras peticiones
 */
 
 //Una buena practica es tener la URL del API en una variable
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=100";
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=200";
 
 //Podemos crear una funcion generica la cual se encargue de hacer la peticion
 // como parametro vamos a recibir la URL que puede ser la que lista o la que nos da el detalle
 // url sera el parametro que reciba la url hacia donde se hará la peticion
 
 // mientras no se le asigne un valor a url este sera igual a BASE_URL
-async function getDataFromPokemon(url =BASE_URL){
+export const getDataFromPokemon = async(url =BASE_URL)=>{
     try {
-        
+        //ahora debemos ejecutar el fetch para poder traer la informacion
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.log(error.message)
     }
-}
+};
