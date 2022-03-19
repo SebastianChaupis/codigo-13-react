6e017cb52e4a12a117b2ebe607ed77e061273535
SebastianChaupis <sebaschaupis1@gmail.com>
2022-03-18 23:00:57 -0500

    paises

--- src/pages/Flags/index.js
+++ src/pages/Flags/index.js
@@ -1,24 +1,38 @@
 import { useState, useEffect } from "react";
-import { CardContent, Card, CardMedia, Container, Grid, TextField, MenuItem, FormControl, InputLabel,Select } from "@mui/material";
+import { CardContent, Card, CardMedia, Container, Grid, TextField, MenuItem, FormControl, InputLabel, Select, CircularProgress } from "@mui/material";
 import { getDataFromPokemon } from "../../service";
 
 const Flags = () => {
     const [countries, setCountries] = useState([]);
 
-    const [region,setRegion]=useState([]);
+    const [region, setRegion] = useState([]);
 
     const fetchCountries = async () => {
-        const response = await getDataFromPokemon("https://restcountries.com/v2/all");
+        const response = await getDataFromPokemon("https://restcountries.com/v3.1/all");
         setCountries(response);
     }
 
-    const handleRegion =async(e)=> {
+    const handleRegion = async (e) => {
         setRegion(e.target.value);
-
+        if (e.target.value == "all") {
+            fetchCountries();
+            return;
+        }
         //Primero limpiamos contries para poder llenarlo con la nueva informacion
 
-        const response = await getDataFromPokemon(`https://restcountries.com/v2/region/${e.target.value}`);
+        const response = await getDataFromPokemon(`https://restcountries.com/v3.1/region/${e.target.value}`);
         setCountries(response);
+    };
+
+    const handleSearchCountry = async(e)=>{
+        const countryName= e.target.value;
+        if(countryName.length==0){
+            fetchCountries();
+        }
+        if(countryName.length>3){
+            const filterCountries = countries.filter((country)=> country.name.official.toUpperCase().includes(countryName.toUpperCase()));
+        setCountries(filterCountries);
+        }
     }
 
     useEffect(() => {
@@ -28,22 +42,24 @@ const Flags = () => {
         <Container>
             <Grid container spacing={3} mt={5}>
                 <Grid item md={6}>
-                    <TextField label="Search for a country..." />
+                    <TextField label="Search for a country..." fullWidth onChange={handleSearchCountry}/>
                 </Grid>
                 <Grid item md={6}>
                     <FormControl fullWidth>
                         <InputLabel>Filter by Region</InputLabel>
                         <Select label="Filter by Region" value={region} onChange={handleRegion}>
+                            <MenuItem value="all">Todas las regiones</MenuItem>
                             <MenuItem value="Africa">Africa</MenuItem>
                             <MenuItem value="Americas">Americas</MenuItem>
                             <MenuItem value="Asia">Asia</MenuItem>
                             <MenuItem value="Europa">Europa</MenuItem>
-                            <MenuItem value="Oceania">Oceania</MenuItem>                            
+                            <MenuItem value="Oceania">Oceania</MenuItem>
                         </Select>
                     </FormControl>
                 </Grid>
                 {
-                    countries.length > 0 && countries.map(country => (
+                    countries.length > 0 ? (
+                        countries.map(country => (
                         <Grid item md={3} xs={12}>
                             <Card>
                                 <CardMedia
@@ -52,15 +68,21 @@ const Flags = () => {
                                     image={country.flags.svg}
                                 />
                                 <CardContent>
-                                    <h4>{country.name}</h4>
+                                    <h4>{country.name.official}</h4>
                                     <p>Population:{country.population}</p>
                                     <p>Region:{country.region}</p>
                                     <p>Capital{country.capital}:</p>
                                 </CardContent>
                             </Card>
                         </Grid>
-                    ))
-                }
+                    )) 
+                    ):(
+                        <div className="center loading">
+                            <CircularProgress/> 
+                            <h4>Cargando</h4>
+                        
+                        </div>
+                    )}
             </Grid>
         </Container>
 
